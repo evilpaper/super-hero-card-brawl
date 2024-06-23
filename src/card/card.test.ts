f1251ae87ff44ddea039c1648e4dbbbcb8097566
Pelle Lundgren <pellburt@gmail.com>
2024-06-23 22:36:44 +0200

    Add method to flip card

--- src/card/card.test.ts
+++ src/card/card.test.ts
@@ -1,6 +1,19 @@
 import { expect, test } from "vitest";
 import Card from "./card";
 
+/**
+ * What properties should a card have?
+ * 1. suite
+ * 2. rank
+ * 3. value
+ * 4. name
+ * 5. element
+ *
+ * What should we be able to do with a card?
+ * 1. CreateHTMLElement (to DOM)
+ * 2. Flip it
+ */
+
 const card = new Card("♠︎", "9", 9, "Wolverine");
 
 test("Card get initialised with correct properties", () => {
@@ -9,7 +22,12 @@ test("Card get initialised with correct properties", () => {
     rank: "9",
     suite: "♠︎",
     value: 9,
-    element: null,
     isFlipped: false,
+    element: undefined,
   });
+});
+
+test("Card can get fliped", () => {
+  card.flip();
+  expect(card.isFlipped).toEqual(true);
 });
--- src/card/card.ts
+++ src/card/card.ts
@@ -1,31 +1,29 @@
 import { Suite, Rank, Value } from "./card.types";
 
-/**
- * What properties should a card have?
- * 1. suite
- * 2. rank
- * 3. value
- * 4. name
- * 5. element
- *
- * What should I be able to do with a card?
- * 1. createHTMLElement (to DOM)
- */
-
 export default class Card {
   suite: Suite;
   rank: Rank;
   value: Value;
   name: string;
-  element: HTMLDivElement | null;
   isFlipped: boolean;
+  element?: HTMLDivElement;
 
-  constructor(suite: Suite, rank: Rank, value: Value, name: string) {
+  constructor(
+    suite: Suite,
+    rank: Rank,
+    value: Value,
+    name: string,
+    element?: HTMLDivElement
+  ) {
     this.suite = suite;
     this.rank = rank;
     this.value = value;
     this.name = name;
-    this.element = null;
+    this.element = element;
     this.isFlipped = false;
+  }
+
+  flip() {
+    this.isFlipped = true;
   }
 }
