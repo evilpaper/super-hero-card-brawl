7861d5880870f8143b046405f15d33a91f20c680
Pelle Lundgren <pellburt@gmail.com>
2024-11-23 20:29:47 +0100

    Render on click

--- src/view/view.ts
+++ src/view/view.ts
@@ -1,35 +1,41 @@
 import Game from "../game/game";
 
 export default class View {
-    private game: Game;
-    
-    constructor(game: Game) {
-        this.game = game;
-    }
+  private game: Game;
 
-    render() {
-        const boardElement = document.getElementById("board");
-        
-        if (boardElement) {
-            boardElement.innerHTML = "";
-            
-            this.game.board.cards.forEach((card) => {
-                const cardElement = document.createElement("li");
-                cardElement.innerHTML = `
+  constructor(game: Game) {
+    this.game = game;
+  }
+
+  render() {
+    const boardElement = document.getElementById("board");
+
+    if (boardElement) {
+      boardElement.innerHTML = "";
+
+      this.game.board.cards.forEach((card) => {
+        const cardElement = document.createElement("li");
+        cardElement.innerHTML = `
                     <li class="card">
                         <button>
-                            <span class="suite">${card.suite}</span>
-                            <span>${card.value}</span>
+                            <div>
+                                <span class="suite">${card.suite}</span>
+                                <span>${card.value}</span>
+                            </div>
+                             <span>${card.played}</span>
                         </button>
                     </li>
                 `;
 
-                if (!card.played) {
-                    cardElement.addEventListener("click", () => card.play());
-                }
-                
-                boardElement.appendChild(cardElement);
-            });
+        if (!card.played) {
+          cardElement.addEventListener("click", () => {
+            card.play();
+            this.render();
+          });
         }
+
+        boardElement.appendChild(cardElement);
+      });
     }
+  }
 }
