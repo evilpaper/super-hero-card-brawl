cd5aab920377cf449552045b58f29fa03e39f708
Pelle Lundgren <pellburt@gmail.com>
2024-12-08 09:27:34 +0100

    Padd test for selecting two healers in a row

--- src/player/player.ts
+++ src/player/player.ts
@@ -3,12 +3,14 @@ export default class Player {
   _defence: number;
   _stamina: number;
   _canMoveOn: boolean;
+  _canHeal: boolean;
 
   constructor() {
     this._health = 21;
     this._defence = 0;
     this._stamina = 0;
     this._canMoveOn = true;
+    this._canHeal = true;
   }
 
   get health(): number {
@@ -43,13 +45,31 @@ export default class Player {
     this._canMoveOn = value;
   }
 
+  get canHeal(): boolean {
+    return this._canHeal;
+  }
+
+  set canHeal(value: boolean) {
+    this._canHeal = value;
+  }
+
   playDefensiveBrawler(potionStrength: number): void {
-    this._health = Math.min(this._health + potionStrength, 21);
+    if (this.canHeal) {
+      this._health = Math.min(this._health + potionStrength, 21);
+      this.canHeal = false;
+    } else {
+      this.canHeal = true;
+    }
   }
 
   playOffensiveBrawler(defenderStrength: number): void {
     this._defence = defenderStrength;
     this.stamina = 22;
+
+    // If canHeal is false, reset it to true
+    if (!this.canHeal) {
+      this.canHeal = true;
+    }
   }
 
   brawl(opponentValue: number): void {
@@ -62,6 +82,10 @@ export default class Player {
 
       this.health = Math.max(0, this.health - damage);
       this.stamina = opponentValue;
+    }
+    // If canHeal is false, reset it to true
+    if (!this.canHeal) {
+      this.canHeal = true;
     }
   }
 }
