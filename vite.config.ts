6fd8322ab4a9f60f5dabac8cd9ef424d21799ff9
Pelle Lundgren <pellburt@gmail.com>
2024-12-14 14:15:20 +0100

    Clarify reason for using base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/superherocardbrawl/", // Replace with your subdirectory if any
+  /**
+   * Vite by default assumes that our project will be served from the root (/) of the server.
+   * Since we serve it from a subdirectory (e.g., ourdomain.com/superherocardbrawl),
+   * the paths to assets like images, CSS, and JavaScript files may break if we don't specify the exact base.
+   * */
+  base: "/superherocardbrawl/", // Replace with subdirectory if any
 });
