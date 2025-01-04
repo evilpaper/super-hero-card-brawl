1fac1d444337fc1581f2398bcef25450bc893494
Pelle Lundgren <pellburt@gmail.com>
2025-01-04 18:56:44 +0100

    Add animation for clover icon

--- src/view/view.ts
+++ src/view/view.ts
@@ -133,6 +133,7 @@ export default class View {
   private defenseElement: HTMLElement | null;
   private defenseFill: HTMLElement | null;
   private staminaElement: HTMLElement | null;
+  private cloverFill: HTMLElement | null;
   private slot1Element: HTMLElement | null;
   private slot2Element: HTMLElement | null;
   private slot3Element: HTMLElement | null;
@@ -151,6 +152,7 @@ export default class View {
     this.defenseFill = document.getElementById("tile-background-rect-fill");
 
     this.staminaElement = document.getElementById("stamina");
+    this.cloverFill = document.getElementById("clover-background-rect-fill");
 
     this.slot1Element = document.querySelector(".slot1");
     this.slot2Element = document.querySelector(".slot2");
@@ -337,6 +339,12 @@ export default class View {
         100 - (this.game.player.getDefence() / 11) * 100
       );
       this.defenseFill.style.transform = `translateY(${percentage}%)`;
+    }
+    if (this.cloverFill) {
+      const percentage = Math.floor(
+        100 - (this.game.player.getStamina() / 22) * 100
+      );
+      this.cloverFill.style.transform = `translateY(${percentage}%)`;
     }
 
     this.animateStatChange(this.defenseElement, this.game.player.getDefence());
