01865674ae0f77c6343322a35cb71f081353bc82
Pelle Lundgren <pellburt@gmail.com>
2025-03-05 20:54:12 +0100

    Only animate can move when transition from disable to enabled

--- src/view/view.ts
+++ src/view/view.ts
@@ -417,36 +417,37 @@ export default class View {
      */
 
     if (this.actionButton) {
+      const wasDisabled = this.actionButton.classList.contains("disabled");
       if (!this.game.player.getCanMoveOn()) {
         this.actionButton.classList.add("disabled");
       } else {
-        this.actionButton.style.transformOrigin = "50% 100%";
-        animate(
-          this.actionButton,
-          {
-            rotate: [
-              "0deg",
-              "2deg",
-              "-4deg",
-              "4deg",
-              "-4deg",
-              "4deg",
-              "-4deg",
-              "2deg",
-              "-2deg",
-              "2deg",
-              "0deg",
-            ],
-          },
-          {
-            duration: 1,
-            ease: "easeInOut",
-          }
-        ).then(() => {
-          if (this.actionButton) {
-            this.actionButton.classList.remove("disabled");
-          }
-        });
+        this.actionButton.classList.remove("disabled");
+        if (wasDisabled) {
+          // Only animate if it was previously disabled
+          this.actionButton.style.transformOrigin = "50% 100%";
+          animate(
+            this.actionButton,
+            {
+              x: [
+                "0px",
+                "4px",
+                "-6px",
+                "6px",
+                "-6px",
+                "6px",
+                "-6px",
+                "4px",
+                "-4px",
+                "2px",
+                "0px",
+              ],
+            },
+            {
+              duration: 1,
+              ease: "easeInOut",
+            }
+          );
+        }
       }
 
       if (this.game.player.getHealth() <= 0) {
